bfaedc332583b10db87d219eb06f38d617c47b21
ssr-devops <surendar,dev08@gmail.com>
2026-02-26 19:40:00 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "example" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+}
+
+resource "aws_security_group" "Allow-all" {
+    name = "Allow-all"
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+      Name = "allow-all-terraform"
+    }
+
+}
