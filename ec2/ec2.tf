673383e4b42bbaff178ec8659ffb5397c6e750cd
ssr-devops <surendar,dev08@gmail.com>
2026-02-26 22:35:57 +0530

    terraform practice

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
     ami = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow-all.id]
 }
 
 resource "aws_security_group" "allow-all" {
